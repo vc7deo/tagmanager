6e44a200b7cbe2f7fa674d77d0bf9fd3e49da80f
Vinod Legacit <vinod@legacit.com>
2023-03-10 12:58:41 +0530

    modified tags

--- pages/_app.js
+++ pages/_app.js
@@ -1,46 +1,41 @@
-import Head from 'next/head'
-import { useRouter } from 'next/router'
-import Script from 'next/script'
-import { useEffect } from 'react'
-import * as gtag from '../lib/gtag'
+import Head from "next/head";
+import { useRouter } from "next/router";
+import Script from "next/script";
+import { useEffect } from "react";
+import * as gtag from "../lib/gtag";
 
 const App = ({ Component, pageProps }) => {
-  const router = useRouter()
+  const router = useRouter();
   useEffect(() => {
     const handleRouteChange = (url) => {
-      gtag.pageview(url)
-    }
-    router.events.on('routeChangeComplete', handleRouteChange)
+      gtag.pageview(url);
+    };
+    router.events.on("routeChangeComplete", handleRouteChange);
     return () => {
-      router.events.off('routeChangeComplete', handleRouteChange)
-    }
-  }, [router.events])
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, [router.events]);
 
   return (
     <>
-      <Head>
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-
-              gtag('config', '${gtag.GA_TRACKING_ID}', {
-                page_path: window.location.pathname,
-              });
-            `,
-          }}
-        />
-      </Head>
-      {/* Global Site Tag (gtag.js) - Google Analytics */}
+      <Head />
       <Script
         strategy="afterInteractive"
         src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`}
       />
+      <Script
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','${gtag.GA_TRACKING_ID}');`,
+        }}
+      ></Script>
       <Component {...pageProps} />
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,21 @@
 import Page from "../components/Page";
 
 export default function Home() {
+  const buynow = (e) => {
+    window.dataLayer.push({
+      event: "custom_buy_now",
+      eventTarget: e.target.id,
+      eventAction: e.type,
+    });
+    console.log(e.target.id);
+  };
   return (
     <Page>
       <h1>This is the Home page</h1>
       <button id="add_cart">Add to Cart</button>
+      <button id="buy_now_button" onClick={buynow}>
+        Buy Now
+      </button>
     </Page>
   );
 }
